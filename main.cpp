af84f471876a3ab689d18c0784a2768eaeea9dc5
Nahuel Ferreiro <n.ferreiro88@gmail.com>
2021-09-27 07:04:06 +0000

    convertion to double (tofix)

--- main.cpp
+++ main.cpp
@@ -10,9 +10,9 @@ using namespace std;
 
 #include "filter.h"
 
-void short_to_double(short *in, double *out, size_t n){
+void short_to_double(int16_t *in, double *out, size_t n){
     for(size_t i = 0; i < n; i++)
-        out[i] = 10.*in[i]/32767.;
+        out[i] = double(in[i])*20./65536.;
 }
 
 int main(int argc, char** argv) {
@@ -41,19 +41,19 @@ int main(int argc, char** argv) {
 
     of = load_filter(filterdata.c_str());
 
-    short rawbuff[of.h.length];
+    int16_t rawbuff[of.h.length];
     double conv_buff[of.h.length];
-    short *bptr = &rawbuff[0];
+    int16_t *bptr = &rawbuff[0];
     
     int req_b=of.h.length*sizeof(short);
 
     while( (n = read(STDIN_FILENO, (char*)bptr, req_b) ) > 0){
         bptr = &rawbuff[n % of.h.length];
-        req_b = n % of.h.length*sizeof(short);
+        req_b = n % of.h.length*sizeof(int16_t);
         if(req_b==0){
-            req_b = of.h.length*sizeof(short);
+            req_b = of.h.length*sizeof(int16_t);
             short_to_double(rawbuff, conv_buff, of.h.length);
-            m = write(STDOUT_FILENO, (char*)bptr, n);
+            m = write(STDOUT_FILENO, (char*)conv_buff, of.h.length);
         }
     }
 
